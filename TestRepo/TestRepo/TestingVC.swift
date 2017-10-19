37b5aad6dbfc23b1a79ea5805c8c7d5548867d00
Bogdan Todea <bogdan.todea@3pillarglobal.com>
2017-10-19 15:50:21 +0300

    Test Feature done

---
+++ TestRepo/TestRepo/TestingVC.swift
@@ -0,0 +1,13 @@
+//
+//  TestingVC.swift
+//  TestRepo
+//
+//  Created by Bogdan Todea on 10/19/17.
+//  Copyright © 2017 3PillarGlobal. All rights reserved.
+//
+
+import UIKit
+
+class TestingVC: NSObject {
+
+}
